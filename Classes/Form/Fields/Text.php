563d26bc5b63ba29b808fd7bb2667f743a26e54e
Tim Büschken <bueschken@team-digital.de>
2018-10-18 15:32:59 +0200

    [UPDATE] Override Flux Function for RTE Presets Configuration

--- Classes/Form/Fields/Text.php
+++ Classes/Form/Fields/Text.php
@@ -2,11 +2,8 @@
 namespace TeamDigital\Sitepackage\Form\Fields;
 
 class Text extends \FluidTYPO3\Flux\Form\Field\Text {
-    public function buildConfiguration()
-    {
-        if (true === $this->getEnableRichText()) {
-            $configuration['richtextConfiguration'] = 'sitepackage';
-        }
-        return $configuration;
+
+    public function getRichtextConfiguration() {
+        return 'sitepackage';
     }
 }

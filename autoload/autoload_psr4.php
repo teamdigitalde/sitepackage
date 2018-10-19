49d13f14c97dab24e58c813f520347806102c6f3
Tim Büschken <bueschken@team-digital.de>
2018-10-19 08:09:39 +0200

    [UPDATE] Add VHS to Templates and Autoload

--- autoload/autoload_psr4.php
+++ autoload/autoload_psr4.php
@@ -7,7 +7,7 @@ $typo3InstallDir = \TYPO3\CMS\Core\Core\Environment::getPublicPath() . '/';
 return array(
     'ArminVieweg\\Dce\\' => array($typo3InstallDir . 'typo3conf/ext/dce/Classes'),
     'FluidTYPO3\\Flux\\' => array($typo3InstallDir . 'typo3conf/ext/flux/Classes'),
-    'GridElementsTeam\\Gridelements\\' => array($typo3InstallDir . 'typo3conf/ext/gridelements/Classes'),
+    'FluidTYPO3\\Vhs\\' => array($typo3InstallDir . 'typo3conf/ext/vhs/Classes'),
     'Netresearch\\RteCKEditorImage\\' => array($typo3InstallDir . 'typo3conf/ext/rte_ckeditor_image/Classes'),
     'TeamDigital\\Bootstrapslider\\' => array($typo3InstallDir . 'typo3conf/ext/bootstrapslider/Classes'),
     'TeamDigital\\Sitepackage\\' => array($typo3InstallDir . 'typo3conf/ext/sitepackage/Classes'),

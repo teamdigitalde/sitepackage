ba92d8c092448262e6db2276d5000b82195d26e1
Tim Büschken <bueschken@team-digital.de>
2018-11-01 08:43:33 +0100

    [UPDATE] Variable for currentLanguage and change styles.content.get to lib.dynamicContent

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -22,6 +22,7 @@ tt_content.stdWrap.innerWrap.cObject {
 	200.value = <div class="example2">|</div>
 }
 
+/*
 ### Einbindung headerbild / Headerslider ###
 lib.headerslideshow < styles.content.get
 lib.headerslideshow {
@@ -29,6 +30,7 @@ lib.headerslideshow {
 	stdWrap.required = 1
 	slide = -1
 }
+*/
 
 lib.backend_layout = TEXT
 lib.backend_layout {
@@ -71,6 +73,11 @@ lib.sitepid {
 lib.currentPid = TEXT
 lib.currentPid.value = {TSFE:id}
 lib.currentPid.insertData = 1
+
+# Aktuelle Sprache im Frontend auslesen
+lib.currentLanguage = TEXT
+lib.currentLanguage.value = {TSFE:sys_language_uid}
+lib.currentLanguage.insertData = 1
 
 # BODY-Tag erstellen ###############################
 lib.bodyTag = COA
@@ -146,6 +153,100 @@ lib.hreflang {
 	if.isTrue = {$headerData.hreflang.active}
 }
 
+lib.dynamicContent = COA
+lib.dynamicContent {
+	10 = LOAD_REGISTER
+	10 {
+		colPos.cObject = TEXT
+		colPos.cObject {
+			field = colPos
+			ifEmpty.cObject = TEXT
+			ifEmpty.cObject {
+				value.current = 1
+				ifEmpty = 0
+			}
+		}
+		pageUid.cObject = TEXT
+		pageUid.cObject {
+			field = pageUid
+			ifEmpty.data = TSFE:id
+		}
+		contentFromPid.cObject = TEXT
+		contentFromPid.cObject {
+			data = DB:pages:{register:pageUid}:content_from_pid
+			data.insertData = 1
+		}
+		wrap.cObject = TEXT
+		wrap.cObject {
+			field = wrap
+		}
+	}
+	20 = CONTENT
+	20 {
+		table = tt_content
+		select {
+			includeRecordsWithoutDefaultTranslation = 1
+			orderBy = sorting
+			where = {#colPos}={register:colPos}
+			where.insertData = 1
+			pidInList.data = register:pageUid
+			pidInList.override.data = register:contentFromPid
+		}
+		stdWrap {
+			dataWrap = {register:wrap}
+			required = 1
+		}
+	}
+	30 = RESTORE_REGISTER
+}
+lib.headerslideshow = COA
+lib.headerslideshow {
+	10 = LOAD_REGISTER
+	10 {
+		colPos.cObject = TEXT
+		colPos.cObject {
+			field = colPos
+			ifEmpty.cObject = TEXT
+			ifEmpty.cObject {
+				value.current = 1
+				ifEmpty = 0
+			}
+		}
+		pageUid.cObject = TEXT
+		pageUid.cObject {
+			field = pageUid
+			ifEmpty.data = TSFE:id
+		}
+		contentFromPid.cObject = TEXT
+		contentFromPid.cObject {
+			data = DB:pages:{register:pageUid}:content_from_pid
+			data.insertData = 1
+		}
+		wrap.cObject = TEXT
+		wrap.cObject {
+			field = wrap
+		}
+	}
+	20 = CONTENT
+	20 {
+		table = tt_content
+		select {
+			includeRecordsWithoutDefaultTranslation = 1
+			orderBy = sorting
+			where = {#colPos}=3
+			where.insertData = 1
+			pidInList.data = register:pageUid
+			pidInList.override.data = register:contentFromPid
+		}
+		stdWrap {
+			dataWrap = {register:wrap}
+			required = 1
+		}
+		slide = -1
+	}
+	30 = RESTORE_REGISTER
+}
+
 # Make the PAGE object
 page = PAGE
 page {
@@ -181,17 +282,18 @@ page {
 		variables {
 			# Current Pid
 			currentPid < lib.currentPid
+			currentLanguage < lib.currentLanguage
 
 			# Insert content as already constructed in TypoScript objects into subparts
-			content_col0 < styles.content.get
-			content_col1 < styles.content.get
-			content_col1.select.where = colPos=1
-			content_col2 < styles.content.get
-			content_col2 {
-				select.where = colPos=2
-				stdWrap.required = 1
-				slide = -1
-			}
+			#content_col0 < styles.content.get
+			#content_col1 < styles.content.get
+			#content_col1.select.where = colPos=1
+			#content_col2 < styles.content.get
+			#content_col2 {
+			#	select.where = colPos=2
+			#	stdWrap.required = 1
+			#	slide = -1
+			#}
 		}
 		dataProcessing {
 			10 = TYPO3\CMS\Frontend\DataProcessing\MenuProcessor

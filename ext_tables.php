b113023342b3a47825f200d4327bd99d0a6b5b68
Tim Büschken <bueschken@team-digital.de>
2019-05-08 11:46:48 +0200

    [BUGFIX] Remove ExtensionManagementUtility gedöns

--- ext_tables.php
+++ ext_tables.php
@@ -1,6 +1,5 @@
 <?php
 defined('TYPO3_MODE') || die();
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/page.ts">'
